e6158cd0061ff9a61e488b9de25a6cd4200a4ef3
ozgur gundogan <ozgurgundogan90@gmail.com>
2018-01-19 17:33:00 +0300

    there are some big problems in max algo

--- tests/maxtest.py
+++ tests/maxtest.py
@@ -1,13 +1,17 @@
 from main.Intersections import *
 
-list1 = sorted([1, 2, 3, 4, 5])
 
-list2 = sorted([1, 3, 4, 2])
+list3 = sorted([0, 2, 8 , 15])
 
-list3 = sorted([0, 2, 3])
+list2 = sorted([11 , 15 , 17 , 36 , 47])
 
-list4 = sorted([1, 2, 5, 6, 7, 3])
+list1 = sorted([1, 2, 3, 4, 5 , 15])
+
+list4 = sorted([1, 2, 5, 6, 7, 3 ,15])
+# list4 = sorted([1, 2, 5, 6, 3, 8])
 
 intersector = Intersections(MAXIntersection)
+
+
 
 print intersector.intersect([list1, list2, list3, list4])
